f150e4c522d1dbbeed442b08b963a539cd3f2b9f
Jeremy Chan <jeremych@outlook.com>
2017-02-12 18:36:30 +0000

    moved around templates, added design report pdf

--- software/web_interface/web_interface/settings.py
+++ software/web_interface/web_interface/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'web_interface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
