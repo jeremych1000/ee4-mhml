7341ad18a1261869899145ae1d4ac9c6541b6fc4
DominicCYK <a23303538@gmail.com>
2017-02-13 11:31:00 +0000

    upload

--- software/backend/backend/settings.py
+++ software/backend/backend/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MLBlock',
-    'lists'
+    'lists',
+    'chat'
 ]
 
 MIDDLEWARE = [
--- software/backend/lists/forms.py
+++ software/backend/lists/forms.py
@@ -4,10 +4,10 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "you can't have an empty item"
 
 
-class text_form(forms.Form):
+class text_form(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('text')
+        fields = ('text',)
         widgets = {'text': forms.fields.TextInput(attrs={'placeholder': 'Enter a to-do item',
                                                          'class': 'form-control input-lg'})}
         error_messages = {
