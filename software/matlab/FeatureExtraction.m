45e977724f788ae8b5b323f616b861a85dc5b5e0
DominicCYK <a23303538@gmail.com>
2017-02-13 11:51:02 +0000

    commit

---
+++ software/matlab/FeatureExtraction.m
@@ -0,0 +1,8 @@
+AccDataPath='../../data/TH_sleep_data/MSBand2_Acc_data_24_01_17.csv'
+GSRDataPath='../../data/TH_sleep_data/MSBand2_Acc_data_24_01_17.csv'
+HRDataPath='../../data/TH_sleep_data/MSBand2_Acc_data_24_01_17.csv'
+SkinDataPath='../../data/TH_sleep_data/MSBand2_Acc_data_24_01_17.csv'
+GSRData=csvread(AccDataPath);
+AccData=csvread(GSRDataPath);
+HRData=csvread(HRDataPath);
+SkinData=csvread(SkinDataPath);
