22aca42ad92182c213b9ed776e0488bcfe69cce8
DominicCYK <a23303538@gmail.com>
2017-02-20 10:21:45 +0000

    upload matlab

--- software/matlab/FeatureExtraction.m
+++ software/matlab/FeatureExtraction.m
@@ -1,19 +1,26 @@
-DataPath='../../data/TH_sleep_data/MSBand2_ALL_data_10.02.17.csv';
+filelist =dir('./*.csv');
+featureVec=[];
+for file_i =1:length(filelist)
+DataPath=[filelist(file_i).folder,'/',filelist(file_i).name];
 [num,text,raw]=xlsread(DataPath,1);
 [m n] = size(raw)
 win_size = 600
 n_win = round(m/win_size)
 raw= raw(2:end,:);
+label = raw{1,end};
 for index = 1: n_win-1
     slice= raw((index-1)*win_size+1:index*win_size,:);
-    mean_hr(index) = mean(cell2mat(slice(:,2)));
-    std_hr(index) = std(cell2mat(slice(:,2)));
-    mean_rr(index) = mean(cell2mat(slice(:,3)));
-    std_rr(index) = std(cell2mat(slice(:,3)));
-    mean_gsr(index)= mean(cell2mat(slice(:,5)));
-    std_gsr(index) =std(cell2mat(slice(:,5)));
-    mean_temp(index)= mean(cell2mat(slice(:,6)));
-    std_temp(index)= std(cell2mat(slice(:,6)));
-    mean_acc(index)= abs(mean(cell2mat(slice(:,7))))^2 +abs(mean(cell2mat(slice(:,8))))^2+abs(mean(cell2mat(slice(:,9))))^2;
+    feature_vec(index,1) = mean(cell2mat(slice(:,2)));
+    feature_vec(index,2) = std(cell2mat(slice(:,2)));
+    feature_vec(index,3) = mean(cell2mat(slice(:,3)));
+    feature_vec(index,4) = std(cell2mat(slice(:,3)));
+    feature_vec(index,5)= mean(cell2mat(slice(:,5)));
+    feature_vec(index,6) =std(cell2mat(slice(:,5)));
+    feature_vec(index,7)= mean(cell2mat(slice(:,6)));
+    feature_vec(index,8)= std(cell2mat(slice(:,6)));
+    feature_vec(index,9)= abs(mean(cell2mat(slice(:,7))))^2 +abs(mean(cell2mat(slice(:,8))))^2+abs(mean(cell2mat(slice(:,9))))^2;
+    feature_vec(index,10) = label;
 end
-feature_vec=[mean_hr,std_hr,mean_rr,std_rr,mean_gsr,std_gsr,mean_temp,std_temp,mean_acc]
+    featureVec = [featureVec;feature_vec];
+%feature_vec=[mean_hr,std_hr,mean_rr,std_rr,mean_gsr,std_gsr,mean_temp,std_temp,mean_acc]
+end
