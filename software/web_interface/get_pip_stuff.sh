cfa85c6e233416ebbaa763e728cff7095b67972d
Jeremy Chan <jeremych@outlook.com>
2017-02-25 22:23:07 +0000

    forgot linux version

---
+++ software/web_interface/get_pip_stuff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip3 install django
+pip3 install django-bootstrap3
+pip3 install django-passwords
+pip3 install djangorestframework
+pip3 install django-sendfile
+pip3 install Sphinx
+pip3 install django-docs
+pip3 install scikit-learn
+pip3 install django-rest-auth
+pip3 install djangorestframework-jwt
+pip3 install django-push-notifications
