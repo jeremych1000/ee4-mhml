8045abda58965b7e97f58e5425c90baef3f9eaca
Jeremy Chan <jeremych@outlook.com>
2017-03-08 01:40:16 +0000

    merge

---
+++ software/mhml_withbackground_working/mhml/RegisterPageViewController.swift
@@ -0,0 +1,114 @@
+//
+//  RegisterPageViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Sergey Kargopolov on 2015-01-13.
+//  Copyright (c) 2015 Sergey Kargopolov. All rights reserved.
+//
+
+/*
+import UIKit
+import Parse
+ 
+
+class RegisterPageViewController: UIViewController {
+
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func registerButtonTapped(_ sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTextField.text;
+        let userRepeatPassword = repeatPasswordTextField.text;
+        
+        // Check for empty fields
+        if((userEmail?.isEmpty)! || (userPassword?.isEmpty)! || (userRepeatPassword?.isEmpty)!)
+        {
+            
+            // Display alert message 
+            
+            displayMyAlertMessage("All fields are required");
+            
+            return;
+        }
+        
+        //Check if passwords match 
+        if(userPassword != userRepeatPassword)
+        {
+           // Display an alert message 
+            displayMyAlertMessage("Passwords do not match");
+            return;
+        
+        }
+      
+        
+     
+        
+        // Store data 
+        let myUser: PFUser = PFUser()
+        
+         myUser.username = userEmail
+         myUser.password = userPassword
+         myUser.email = userEmail
+        
+      
+         myUser.signUpInBackgroundWithBlock {
+            (success:Bool!, error:NSError!) -> Void in
+            
+                println("User successfully registered")
+       
+            // Display alert message with confirmation.
+            var myAlert = UIAlertController(title:"Alert", message:"Registration is successful. Thank you!", preferredStyle: UIAlertControllerStyle.Alert);
+            
+            let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.Default){ action in
+                self.dismissViewControllerAnimated(true, completion:nil);
+            }
+            
+            myAlert.addAction(okAction);
+            self.presentViewController(myAlert, animated:true, completion:nil);
+     
+                
+        }
+        
+        
+ 
+      
+        
+        
+    }
+    
+    
+    func displayMyAlertMessage(_ userMessage:String)
+    {
+      
+        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.default, handler:nil);
+        
+        myAlert.addAction(okAction);
+
+        self.present(myAlert, animated:true, completion:nil);
+        
+    }
+ 
+    @IBAction func iHaveAnAccountButtonTapped(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+ 
+ */
